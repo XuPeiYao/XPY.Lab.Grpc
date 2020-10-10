c1101c9970e569342e69511a8f4cf4b1debe537f
XuPeiYao <st89382000@gmail.com>
2020-10-10 19:22:43 +0800

    加入專案檔案。

---
+++ XPY.Lab.Grpc.Client/Program.cs
@@ -0,0 +1,31 @@
+﻿using Grpc.Net.Client;
+
+using System;
+using System.Threading.Tasks;
+
+using XPY.Lab.Grpc.Client.Protos;
+
+using static XPY.Lab.Grpc.Client.Protos.Sample;
+
+namespace XPY.Lab.Grpc.Client
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            await Task.Delay(1000 * 5); // wait service boot
+            // The port number(5001) must match the port of the gRPC server.
+            using var channel = GrpcChannel.ForAddress("https://localhost:5001");
+            var client = new Sample.SampleClient(channel);
+
+            var reply = client.Login(
+                              new SampleRequest
+                              {
+                                  Account = "user",
+                                  Password = "1234"
+                              });
+            Console.WriteLine("Login Result: " + reply.Verified);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ XPY.Lab.Grpc/Services/SampleService.cs
@@ -0,0 +1,19 @@
+﻿using Grpc.Core;
+
+using System.Threading.Tasks;
+
+using XPY.Lab.Grpc.Protos;
+
+namespace XPY.Lab.Grpc.Services
+{
+    public class SampleService : Sample.SampleBase
+    {
+        public override async Task<SampleResponse> Login(SampleRequest request, ServerCallContext context)
+        {
+            return new SampleResponse()
+            {
+                Verified = request.Account == "user" && request.Password == "1234"
+            };
+        }
+    }
+}
